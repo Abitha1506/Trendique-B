8a36a18e1134bef1d0ede59eb593d654c06c4521
Subash RajaRathinam <subash1968raja@gmail.com>
2025-07-05 22:39:52 +0530

    prjt

--- Server.js
+++ Server.js
@@ -37,6 +37,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const bcrypt = require('bcryptjs');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 
